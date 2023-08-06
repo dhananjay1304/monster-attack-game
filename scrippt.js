3b1b3f0137e3876d43acfbcc03563337cdfcd18e
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-08-06 09:20:39 +0530

    add prompt

--- scrippt.js
+++ scrippt.js
@@ -3,6 +3,8 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+prompt('Maximun life for you and the Monster.','100');
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
