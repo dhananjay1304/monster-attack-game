64164bb02d61efb13d5928e731d5f14093bafebd
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-08-06 11:12:40 +0530

    event

--- scrippt.js
+++ scrippt.js
@@ -5,10 +5,16 @@ const HEAL_VALUE = 20;
 
 const MODE_ATTACK = 'ATTACK'; // MODE_ATTACK = 0 
 const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; // MODE_STRONG_ATTACK = 1
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER HEAL';
+const LOG_EVENT_GAME_OVER = 'GAME OVER';
 
 const enteredValue = prompt('Maximun life for you and the Monster.','100');
 
 let chosenMaxLife = parseInt(enteredValue);
+let battlelog = [];
 
 if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
   chosenMaxLife = 100;
@@ -17,11 +23,10 @@ if(isNaN(chosenMaxLife) || chosenMaxLife <= 0){
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
-let battleLog = [];
 
 adjustHealthBars(chosenMaxLife);
 
-function WriteToLog() {
+function WriteToLog(event) {
   
 }
 
